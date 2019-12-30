299c0f1f09b254aa25067aeee2b3e307d5f798e4
hrishabh <rishabhofficial88@gmail.com>
2019-12-30 19:40:45 +0530

    portfolio

---
+++ precache-manifest.2038595e2e4a8a78d7c110dac46f3d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbac01b1b0f840cd2d620a8b8b0f56ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063a7ad10dc075bae56b",
+    "url": "/static/css/main.bbd52057.chunk.css"
+  },
+  {
+    "revision": "26eee67bc8e470bf3286",
+    "url": "/static/js/2.ae49dfbe.chunk.js"
+  },
+  {
+    "revision": "063a7ad10dc075bae56b",
+    "url": "/static/js/main.c3690342.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
